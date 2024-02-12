b81f590f302ed2dce75402b3287550e57d4d620f
Elodie <elodie.loeb.simplon@gmail.com>
2024-02-12 21:54:36 +0100

    sans les consoles log

--- script.js
+++ script.js
@@ -21,18 +21,26 @@ regleJeu.addEventListener("click", () => {
   const paragrapheJeu1 = document.createElement("p");
   const paragrapheJeu2 = document.createElement("p");
   const paragrapheJeu3 = document.createElement("p");
-  const paragrapheJeuInit = document.createElement("p")
-  paragrapheJeuInit.innerText = "Deux images vont s'afficher à l'écran. Choisis le bon bouton pour gagner un maximum de points en 1 minute!"
-  
+  const paragrapheJeuInit = document.createElement("p");
+  paragrapheJeuInit.innerText =
+    "Deux images vont s'afficher à l'écran. Choisis le bon bouton pour gagner un maximum de points en 1 minute!";
+
   paragrapheJeu1.innerText =
     "Si l'image est de la bonne couleur, clique sur le bouton correspondant (La montagne est grise, le crabe est rouge, le fantome est blanc, le livre est bleu et la sucette est verte).";
   paragrapheJeu2.innerText =
-  "Si les deux images sont de la bonne couleur, la deuxième doit être choisie.";
-    
-  paragrapheJeu3.innerText ="Si aucun objet n'est de la bonne couleur, clique sur l'image qui n'est représenté ni par sa forme ni par sa couleur.";
+    "Si les deux images sont de la bonne couleur, la deuxième doit être choisie.";
+
+  paragrapheJeu3.innerText =
+    "Si aucun objet n'est de la bonne couleur, clique sur l'image qui n'est représenté ni par sa forme ni par sa couleur.";
   divRegle.classList.add("divRegle");
   regleJeu.appendChild(divRegle);
-  divRegle.append(titreRegle, paragrapheJeuInit, paragrapheJeu1, paragrapheJeu2, paragrapheJeu3);
+  divRegle.append(
+    titreRegle,
+    paragrapheJeuInit,
+    paragrapheJeu1,
+    paragrapheJeu2,
+    paragrapheJeu3
+  );
   document.addEventListener("click", (event) => {
     if (!regleJeu.contains(event.target)) {
       divRegle.remove();
@@ -42,23 +50,22 @@ regleJeu.addEventListener("click", () => {
 
 boutons.forEach((element) => {
   element.addEventListener("click", () => {
-    console.log(temps)
-    if(temps > 1){
-    if (element.id == "bouton" + formes[bonChiffreDesHasard]) {
-      messageReussite();
-      EcrireScore(IncrementerPoints());
-      setTimeout(() => EnvoiDesCartes(), 1000);
-    } else {
-      messageDefaite();
-      EcrireScore(DecrementerPoints());
-      setTimeout(() => EnvoiDesCartes(), 1000);}
+    if (temps > 1) {
+      if (element.id == "bouton" + formes[bonChiffreDesHasard]) {
+        messageReussite();
+        EcrireScore(IncrementerPoints());
+        setTimeout(() => EnvoiDesCartes(), 1000);
+      } else {
+        messageDefaite();
+        EcrireScore(DecrementerPoints());
+        setTimeout(() => EnvoiDesCartes(), 1000);
+      }
     }
   });
 });
 
-
 function lanceJeu() {
-  temps = 61
+  temps = 61;
   EnvoiDesCartes();
   lancerDecompteTemps();
   boutonLancerJeu.removeEventListener("click", lanceJeu);
@@ -161,7 +168,7 @@ function afficherMessageFindePartie(score) {
   let messageFin = document.createElement("p");
   messageFin.innerText = `Partie terminée, tu as ${score} points`;
   boiteFin.classList.add("messageFinDePartie");
-  messageFin.style.textAlign = "center"
+  messageFin.style.textAlign = "center";
   endroitOuJeu.appendChild(boiteFin);
   boiteFin.appendChild(messageFin);
 }
